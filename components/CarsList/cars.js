d82930a5a9c0befbba395dc7604217c945bb4fc9
Ayushman Dutt Pandey <78679785+Ayushmanpandey@users.noreply.github.com>
2021-06-23 19:54:08 +0530

    Add files via upload
    Audi cars Collection App just for fun scrollable screen application

---
+++ components/CarsList/cars.js
@@ -0,0 +1,25 @@
+export default [
+  {
+    name: "Model T",
+    tagline: "Starting at $69,420",
+    image: require("../../assets/images/ModelS.jpeg"),
+  },
+  {
+    name: "Model E",
+    tagline: "Order Online for",
+    taglineCTA: "Touchless Delivery",
+    image: require("../../assets/images/Model3.jpeg"),
+  },
+  {
+    name: "Model S",
+    tagline: "Order Online for",
+    taglineCTA: "Touchless Delivery",
+    image: require("../../assets/images/ModelX.jpeg"),
+  },
+  {
+    name: "Model L",
+    tagline: "Order Online for",
+    taglineCTA: "Touchless Delivery",
+    image: require("../../assets/images/ModelY.jpeg"),
+  },
+];
